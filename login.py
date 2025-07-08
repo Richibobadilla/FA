e654275f17dd6e57bdaa4972236a9cf3d439b52e
Ricardo Bobadilla Lara <ricardobobadilla1989@gmail.com>
2025-07-08 09:58:57 -0600

    Add files via upload

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.markdown("""
     <style>
     .stApp {
-        background: linear-gradient(to right, #c2e9fb, #a1c4fd);
+        background: linear-gradient(to right, #95b8d1, #7a93c0);
     }
 
     .login-box {
